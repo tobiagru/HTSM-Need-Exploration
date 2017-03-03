0d5e8d94e952497cc926c3feeaa44903d95393dd
Johannes Kern <johannes.kern@cofoundme.org>
2017-03-03 18:35:00 +0100

    added query

--- app/main/src.py
+++ app/main/src.py
@@ -5,19 +5,25 @@ import json
 #import public
 
 #import privat
-from .. import models
+from .. import db
 
 #build a json object with 10 questions where every
 # questions has questionsID, option1 and option2
 def build_questions(language="EN", owner=None):
 
 	#query 20 random questions
-	Session.query.offset(
-       	func.floor(
-           	func.random() *
-            	db.session.query(func.count(model_name.id))
-        	)
-    	).limit(20).all()
+	questions = db.Question.query.join(db.QuestionText).order_by(rand()).limit(20).all()
+	
+
+	# Session.query.offset(
+    #    	func.floor(
+    #        	func.random() *
+    #         	db.session.query(func.count(model_name.id))
+    #     	)
+    # 	).limit(20).all()
+
+
+
 
 def getusertype():
 	random.randint(1,20)
@@ -37,5 +43,3 @@ def save_answers(answers, owner=None):
 			answerId = answer_tmp.
 			parsed_answer[metadata][language]
 			#parsed_answer[metadata][country]
-
-
