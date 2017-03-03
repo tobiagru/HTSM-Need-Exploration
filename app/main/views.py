8e5ce12e6a9519df046192dc7f6b2b49cdf7e7d5
Tobi Grundmann <tobiagru@student.ethz.ch>
2017-03-03 15:08:06 +0100

    "create views getquestions, postanswers, analytics, getquestions/<owner>, postanswers/<owner>"

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,66 @@
-from flask import render_template
+#import core
+
+#import public 
+from flask import render_template, request
 from ..models import EditableHTML
 
+#import privat
 from . import main
+from . import models
+import src
 
+#Session
 
 @main.route('/')
 def index():
     return render_template('main/index.html')
-
 
 @main.route('/about')
 def about():
     editable_html_obj = EditableHTML.get_editable_html('about')
     return render_template('main/about.html',
                            editable_html_obj=editable_html_obj)
+
+#returns: questions
+@main.route('/getquestion', methods=['GET', 'POST'])
+def getquestion():
+	if request.method == 'POST':
+		return build_questions(
+			language = request.form['language'],
+		)
+	if request.method == 'GET':
+		return build_questions()
+
+#returns: questions
+@main.route('/getquestion/<owner>', methods=['GET', 'POST'])
+def getquestion():
+	if request.method == 'POST':
+		return build_questions(
+			language = request.form['language'],
+			owner = owner
+		)
+	if request.method == 'GET':
+		return build_questions(
+			language = "EN",
+			owner = owner)
+
+#recieve: answers, asnwer_meta
+#return: usertype
+@main.route('/postanswer', methods=['GET', 'POST'])
+def postanswer():
+	if request.method == 'POST':
+		save_answers(request.form['answers'])
+		return getusertype()
+
+#recieve: answers, asnwer_meta
+#return: usertype
+@main.route('/postanswer/<owner>', methods=['GET', 'POST'])
+def postanswer():
+	if request.method == 'POST':
+		save_answers(request.form['answers'], owner)
+		return getusertype()
+
+#return: ...
+@main.route('/analytics')
+def analytics():
+	pass
