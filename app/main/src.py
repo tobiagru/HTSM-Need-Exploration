cd8ec0c1f56eab9473fe657c14bfe80fe131f116
Tobi Grundmann <tobiagru@student.ethz.ch>
2017-03-03 20:40:43 +0100

    corrected minor errors and added static fail questions array

--- app/main/src.py
+++ app/main/src.py
@@ -7,6 +7,29 @@ import json
 #import privat
 from .. import db
 
+fail_questions = json.dumps({"questions":[
+							[{"questionId": "1","questionText":"1"},
+								{"questionId": "2","questionText":"2"}],
+							[{"questionId": "3","questionText":"3"},
+								{"questionId": "4","questionText":"4"}],
+							[{"questionId": "5","questionText":"5"},
+								{"questionId": "6","questionText":"6"}],
+							[{"questionId": "7","questionText":"7"},
+								{"questionId": "8","questionText":"8"}],
+							[{"questionId": "9","questionText":"9"},
+								{"questionId": "10","questionText":"10"}],
+							[{"questionId": "11","questionText":"11"},
+								{"questionId": "12","questionText":"12"}],
+							[{"questionId": "13","questionText":"13"},
+								{"questionId": "14","questionText":"14"}],
+							[{"questionId": "15","questionText":"15"},
+								{"questionId": "16","questionText":"16"}],
+							[{"questionId": "17","questionText":"17"},
+								{"questionId": "18","questionText":"18"}]
+							[{"questionId": "19","questionText":"19"},
+								{"questionId": "20","questionText":"20"}],
+							]})
+
 #build a json object with 10 questions where every
 # questions has questionsID, option1 and option2
 def build_questions(language="EN", owner=None):
@@ -28,12 +51,12 @@ def build_questions(language="EN", owner=None):
 							.all()
 	except:
 		print("Failed to load 20 questions from the database in src build_questions")
-		return "fail"
+		return fail_questions
 	
 	questions = {}
 
 	try:
-		questions[questions] = [ 
+		questions["questions"] = [ 
 				[ 
 					{"questionId":question.questionId,
 						 "questionText":question.questionText},
@@ -43,7 +66,13 @@ def build_questions(language="EN", owner=None):
 			]
 	except:
 		print("Failed to turn list of 20 questions into 10 tuples of 2 questions")
-		return "fail"
+		return fail_questions
+
+	try:
+		return json.dumps(questions)
+	except:
+		print("failed to convert dict of questions to json")
+		return fail_questions
 
 	# Session.query.offset(
     #    	func.floor(
