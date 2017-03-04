fd45514abd95942e2fce5a48b23670262ce5843f
Tobi Grundmann <tobiagru@student.ethz.ch>
2017-03-04 11:59:43 +0100

    added missing id

--- app/models/fill_the_db.py
+++ app/models/fill_the_db.py
@@ -56,8 +56,8 @@ def fill_the_db():
 				new_question = Question(owner = "FB")
 				db.session.add(new_question)
 				db.session.commit()
-				new_questionText = QuestionText(questionId = new_question,
-										language = "EN",
+				new_questionText = QuestionText(questionId = new_question.id
+										language = 'EN',
 										text = adjective + " " +noun)
 				db.session.add(new_questionText)
 				db.session.commit()
