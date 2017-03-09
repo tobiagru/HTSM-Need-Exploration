0af752d8cb744b115355bd4a0a8d3ebe5bf40d8f
Johannes Kern <johannes.kern@cofoundme.org>
2017-03-09 10:03:36 +0100

    ripped out url_for static

--- app/utils.py
+++ app/utils.py
@@ -18,16 +18,3 @@ def register_template_utils(app):
 
 def index_for_role(role):
     return url_for(role.index)
-
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
-
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                     endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
