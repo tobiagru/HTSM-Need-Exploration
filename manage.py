cc9f7747f0607fd06fe168ccce50b222f87148c7
Tobi Grundmann <tobiagru@student.ethz.ch>
2017-03-24 06:56:27 +0100

    .

--- manage.py
+++ manage.py
@@ -14,7 +14,7 @@ from app.models import Role, User, fill_the_db, Question, QuestionText, Answer, 
 from app.main.src import build_questions
 
 from sqlalchemy import Float
-from sqlalchemy.sql.expression import func, case, literal_column
+from sqlalchemy.sql.expression import func, case, literal_column, cast
 import json
 
 if os.path.exists('config.env'):
